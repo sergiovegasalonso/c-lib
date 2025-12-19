7d818ba1e96dd85e803d6d06136437c993bc0517
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-12-19 19:34:08 +0100

    added dna validation function

--- lib/code_wars/code_wars.c
+++ lib/code_wars/code_wars.c
@@ -1,5 +1,6 @@
 #include "code_wars.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,7 +15,7 @@ char *dna_to_rna(const char *dna)
     const char THYMINE = 'T';
     const char URACIL = 'U';
 
-    if (dna == NULL)
+    if (!is_valid_dna_sequence(dna))
     {
         return NULL;
     }
@@ -34,4 +35,44 @@ char *dna_to_rna(const char *dna)
     }
 
     return rna;
+}
+
+/**
+ * @brief Validates if the given DNA sequence contains only valid nucleotides.
+ * @param dna Pointer to the input DNA string.
+ * @return true if the sequence is valid, false otherwise.
+ */
+bool is_valid_dna_sequence(const char *dna)
+{
+    if (dna == NULL)
+    {
+        return false;
+    }
+
+    const char ADENINE = 'A';
+    const char THYMINE = 'T';
+    const char CYTOSINE = 'C';
+    const char GUANINE = 'G';
+    const char VALID_NUCLEOTIDES[] = {ADENINE, THYMINE, CYTOSINE, GUANINE};
+    size_t dna_len = strlen(dna);
+
+    for (size_t i = 0; i < dna_len; ++i)
+    {
+        bool is_valid = false;
+        for (size_t j = 0; j < sizeof(VALID_NUCLEOTIDES); ++j)
+        {
+            if (dna[i] == VALID_NUCLEOTIDES[j])
+            {
+                is_valid = true;
+                break;
+            }
+        }
+
+        if (!is_valid)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
--- lib/code_wars/code_wars.h
+++ lib/code_wars/code_wars.h
@@ -1,11 +1,20 @@
 #ifndef CODE_WARS_H
 #define CODE_WARS_H
 
+#include <stdbool.h>
+
 /**
  * @brief DNA to RNA conversion function.
  * @param dna Pointer to the input DNA string.
  * @return Pointer to the newly allocated RNA string.
  */
 char* dna_to_rna(const char* dna);
+
+/**
+ * @brief Validates if the given DNA sequence contains only valid nucleotides.
+ * @param dna Pointer to the input DNA string.
+ * @return true if the sequence is valid, false otherwise.
+ */
+bool is_valid_dna_sequence(const char* dna);
 
 #endif  // CODE_WARS_H
